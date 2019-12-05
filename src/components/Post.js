b51f586e5617ef82d094e12d8961454ccc36c485
Justin Hutsell <hutselljustin@gmail.com>
2019-12-05 17:52:53 -0500

    adds final mvp

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import {  MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCol } from 'mdbreact';
+
 
 class Post extends React.Component{
 
+    render() {
+        console.log(this.props.data)
+        let date = new Date(this.props.post.data.created).toLocaleDateString("en-US")
+        let time = new Date(this.props.post.data.created).toLocaleTimeString("en-US") 
+        const {thumbnail, title, subreddit_name_prefixed, score, ups, num_comments} = this.props.post.data
+        return(
+            <MDBCol >
+                <MDBCard style={{ width: "22rem", height: "28rem", background:"white", border:"solid" }}>
+                    <MDBCardImage className="img-fluid" src={thumbnail ? thumbnail : null} style={{ width: "12rem", height: "12rem" }} waves />
+                    <MDBCardBody>
+                        <MDBCardTitle>{title}</MDBCardTitle>
+                        <p>Date Created: {date} @ {time}</p>
+                        <p>Subreddit: {subreddit_name_prefixed}</p>
+                        <p>Score: {score}</p>
+                        <p>Ups: {ups} </p>
+                        <p># of Comments: {num_comments}</p>
+                    </MDBCardBody>
+                </MDBCard>
+            </MDBCol>
+        )
+    }
 }
 
 export default Post
--- src/components/PostIndex.js
+++ src/components/PostIndex.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Post from './Post'
 
 const REDDIT_ALL = "https://www.reddit.com/r/all/.json"
 
 class PostIndex extends React.Component {
     
     state = {
-        allPosts: []
+        allPosts: [],
+        adult: false,
+        sortTerm: '',
+        sortOrder: true,
+        subReddit: [],
+        subFilterTerm: '',
     }
 
     componentDidMount() {
@@ -15,14 +21,92 @@ class PostIndex extends React.Component {
                 this.setState({
                     allPosts: posts.data.children
                 })
+
+                posts.data.children.forEach(element => {
+                    if (! this.state.subReddit.includes(element.data.subreddit)){
+                    this.setState({
+                        subReddit:[...this.state.subReddit, element.data.subreddit]
+                    })
+                }
+                });
             })
+    }
+
+    filterAdult = (data) => {
+        return data.filter((post) => {
+            return !post.data.over_18
+        })
+    }
+
+    sortFunction = (data) => {
+        let  sortTerm= this.state.sortTerm
+        data = data.sort((el1,el2)=>{           
+            return (el1.data[sortTerm] - el2.data[sortTerm])
+        })
+
+        if(!this.state.sortOrder){
+            data.reverse()
+        }
+        return data
+    }
+
+    filterBySubReddit = (data) => {
+        return data.filter((post) => {
+            return post.data.subreddit === this.state.subFilterTerm
+        })
     }
 
 
 
     render() {
-        console.log(this.state)
-        return <p>hi</p>
+
+        let data = this.state.allPosts
+        if (this.state.adult) {
+            data = this.filterAdult(data)
+        }
+        if(this.state.sortTerm.length > 0){
+            data = this.sortFunction(data)
+        }
+
+        if(this.state.subFilterTerm.length > 0){
+            data = this.filterBySubReddit(data)
+        }
+
+        let eachPost = data.map((post) => {
+            return <Post post={post}/>
+        })
+
+        return(
+            <React.Fragment>
+                <div class="dropdown2" style={{"margin": "50px"}}>
+                    <button class="dropbtn2">Sort By</button>
+                    <div class="dropdown-content2" >
+                        <a onClick={ ()=>{this.setState({sortTerm:"created"})} } href="#">Creation Date</a>
+                        <a onClick={ ()=>{this.setState({sortTerm:"score"})} }href="#">Score</a>
+                        <a onClick={()=>{this.setState({sortTerm:"subreddit_subscribers"})} } href="#">SubReddit Subs</a>
+                        <a onClick={()=>{this.setState({sortTerm:"ups"})}} href="#">Ups</a>
+                        <a onClick={()=>{this.setState({sortTerm:"num_comments"})}} href="#">No of comments</a>
+                    </div>
+                </div>
+
+                <div class="dropdown1">
+                    <button class="dropbtn1">Filter</button>
+                    <div class="dropdown-content1" >
+                        <a onClick={()=>{this.setState({subFilterTerm: ""})}} href="#">All</a>
+                        {this.state.subReddit.map((term)=>{
+                            return <a onClick={ ()=>{this.setState({subFilterTerm:term})} } href="#">{term}</a>
+                        })}
+                    </div>
+                </div>
+            
+                <button onClick={()=>{this.setState({adult:!this.state.adult})}}>Hide 18+ Content</button>
+                <button onClick={()=> this.setState({sortOrder:true})}>accending</button>
+                <button onClick={()=> this.setState({sortOrder:false})}>decending</button>
+                <div  className="posts-container">
+                {eachPost}
+            </div>
+        </React.Fragment>
+        )
     }
 }
 
